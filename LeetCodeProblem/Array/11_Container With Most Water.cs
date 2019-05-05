1e69827d18f02cc1b9655d71cac40b1ba010e8a0
amingzw <giszw@sina.com>
2019-05-05 14:14:51 +0800

    commit

---
+++ LeetCodeProblem/Array/11_Container With Most Water.cs
@@ -0,0 +1,47 @@
+﻿/**
+* 类 名 称  :_11_Container_With_Most_Water
+* 作    者  :zw-pc
+* 命名空间  :LeetCodeProblem.Array
+* CLR 版本  :4.0.30319.42000
+* 创建时间  :2019/4/22 15:38:12
+* 修改时间  :2019/4/22 15:38:12
+*
+* Copyright  @ 2018. All Rights reserved
+*
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem.Array
+{
+    /// <summary>
+    /// 首先计算宽度最大  然后往中间走 宽度变小取高度最大的
+    /// </summary>
+    public class _11_Container_With_Most_Water
+    {
+        public static int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int i = 0;
+            int j = height.Length - 1;
+            while (i < j)
+            {
+                maxArea = Math.Max(Math.Min(height[i], height[j]) * (j - i),maxArea);
+                if(height[i]<=height[j])
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            
+            return maxArea;
+        }
+        
+    }
+}
---
+++ LeetCodeProblem/Array/15_3Sum.cs
@@ -0,0 +1,49 @@
+﻿/**
+* 类 名 称  :_15_3Sum
+* 作    者  :zw-pc
+* 命名空间  :LeetCodeProblem.Array
+* CLR 版本  :4.0.30319.42000
+* 创建时间  :2019/4/26 11:11:23
+* 修改时间  :2019/4/26 11:11:23
+*
+* Copyright  @ 2018. All Rights reserved
+*
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem.Array
+{
+    public class _15_3Sum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<int> pInt = nums.Where((c) => c > 0).ToList();
+            IList<int> zInt = nums.Where((c) => c == 0).ToList();
+            IList<int> nInt = nums.Where((c) => c < 0).ToList();
+
+            
+
+            return null;
+        }
+
+        public static int test()
+        {
+            string[] strList = new string[] { "nesa.id", "rfca.name", "nesa.tt", "trea.tt", "rfca.yy" };
+            var tt = strList.GroupBy(x => x.Split('.')[0], y => y.Split('.')[1])
+                .ToDictionary(group => group.Key, group =>group.ToList());
+            foreach (var t in tt.Keys)
+            {
+                string key = t;
+                foreach (var s in tt[t])
+                {
+                    string d = s;
+                }
+            }
+            return 0;
+        }
+    }
+}
