29fa6b8680973af3a9965f81b45b1eb3280a2c0a
amingzw <giszw@sina.com>
2019-05-05 14:20:25 +0800

    test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -10,13 +10,9 @@ namespace UnitTestProject1
         public void TestMaxArea()
         {
             int[] height = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
-            //Assert.AreEqual(49, LeetCodeProblem.Array._11_Container_With_Most_Water.MaxArea(height));
+            Assert.AreEqual(49, LeetCodeProblem.Array._11_Container_With_Most_Water.MaxArea(height));
         }
-        [Test]
-        public void Test3Sum()
-        {
-            Assert.AreEqual(0, LeetCodeProblem.Array._15_3Sum.test());
-        }
+        
 
     }
 }
